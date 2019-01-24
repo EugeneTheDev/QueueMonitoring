4f571ea11c0e78f5cfda79dae0b0d34e7ff2ff9c
Eugene <eugene.seager@gmail.com>
2019-01-24 23:30:20 +0300

    Init commit

---
+++ src/main/java/com/javathon/sbercalculator/SberCalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.javathon.sbercalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberCalculatorApplication.class, args);
+    }
+
+}
+
