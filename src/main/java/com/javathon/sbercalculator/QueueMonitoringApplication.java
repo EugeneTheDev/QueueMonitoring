0f178064bfbe8c71cecf4374406c8c2afa0725e2
Eugene <eugene.seager@gmail.com>
2019-01-26 17:06:44 +0300

    Rename project

--- src/main/java/com/javathon/sbercalculator/QueueMonitoringApplication.java
+++ src/main/java/com/javathon/sbercalculator/QueueMonitoringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SberCalculatorApplication {
+public class QueueMonitoringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SberCalculatorApplication.class, args);
+        SpringApplication.run(QueueMonitoringApplication.class, args);
     }
 
 }
