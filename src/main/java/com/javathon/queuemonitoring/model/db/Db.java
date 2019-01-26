eea790724c46b8fb843621177b8a6d22c6a472c9
Eugene <eugene.seager@gmail.com>
2019-01-26 19:05:45 +0300

    Add update method for db

--- src/main/java/com/javathon/queuemonitoring/model/db/Db.java
+++ src/main/java/com/javathon/queuemonitoring/model/db/Db.java
@@ -6,8 +6,11 @@ import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Updates.*;
 
 public class Db {
     private MongoCollection<Document> places;
@@ -21,4 +24,34 @@ public class Db {
         placesList.forEach(el -> el.remove("_id"));
         return placesList;
     }
+
+    public void updateInformation(long id, int count){
+        List<Integer> userData = places.find(eq("id", id))
+                .first()
+                .get("userData", ArrayList.class);
+
+        if (userData.size() > 3) {
+            userData.add(count);
+            int averageQueue = 0;
+            for (Integer el : userData) averageQueue+=el;
+            averageQueue = averageQueue / userData.size() + 1;
+
+            places.updateOne(
+                    eq("id", id),
+                    combine(
+                            set("queue", averageQueue),
+                            set("userData", Collections.EMPTY_LIST)
+                    )
+
+            );
+        } else {
+            userData.add(count);
+
+            places.updateOne(
+                    eq("id", id),
+                    set("userData", userData)
+            );
+        }
+    }
+
 }
